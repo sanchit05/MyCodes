13051a85ff6c0dcefb01aae16bb3c98b3c2ba3b2
sanchit05 <45847904+sanchit05@users.noreply.github.com>
2019-10-08 23:32:26 +0530

    log generation
    log generation  program in java

---
+++ log.java
@@ -0,0 +1,37 @@
+package logs;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
+
+public class LoggerExample {
+    private String message;
+    private final static Logger logger= LogManager.getLogger(LoggerExample.class);
+    public void PrintLogs()
+    {
+        Scanner sc=new Scanner(System.in);
+        while (true)
+        {
+            System.out.println("enter message");
+            message=sc.next();
+            logger.info(message);
+            if(message.equals("exit"))
+            {
+                break;
+
+            }
+
+        }
+    }
+}
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       LoggerExample loggerExample= new LoggerExample();
+       loggerExample.PrintLogs();
+    }
+}
