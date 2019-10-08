6eb8068f32c684a6decdf328b6d1fbb9d62e66df
sanchit05 <45847904+sanchit05@users.noreply.github.com>
2019-10-08 23:17:26 +0530

    updated cuims login
    updated cuims login

--- cu_login.py
+++ cu_login.py
@@ -4,13 +4,13 @@ driver=webdriver.Chrome()
 driver.get('https://uims.cuchd.in/uims/')
 
 username_box = driver.find_element_by_id('txtUserId')
-username_box.send_keys('17BCS3842')
+username_box.send_keys('17BCS3842') #your UID
 
 next_button = driver.find_element_by_id('btnNext')
 next_button.click()
                      
 password_box = driver.find_element_by_id('txtLoginPassword')
-password_box.send_keys('Shinchan08@')
+password_box.send_keys('Shinchan08@') #your password
 
 login_button = driver.find_element_by_id('btnLogin')
 login_button.click()
